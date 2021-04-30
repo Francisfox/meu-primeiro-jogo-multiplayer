3690d703eb5a75bca0a898514e311d2455013c2c
Francisfox <francismarfox@hotmail.com>
2021-04-30 18:55:53 +0000

    socket

--- playground/release/server.js
+++ playground/release/server.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
+//import socketio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
+//const sockets = socketio(server)
 
 app.use(express.static('public'))
 
@@ -13,6 +15,11 @@ game.addFruit({fruitId: 'fruit1', fruitX: 4, fruitY: 3})
 game.movePlayer({playerId: 'player1', keyPressed: 'ArrowLeft'})          
 
 console.log(game.state)
+
+//sockets.on('connection', (socket) => {
+//    const playerId = socket.id
+//    console.log(`Player connect on Server with id ${playerId}`)
+//}) 
 
 server.listen(3000, () => {
     console.log(`Server listening on port: 3000`)
