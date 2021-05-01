e069e2ce72d14db4b9d4fcfebcf92df7bb58542a
Francisfox <francismarfox@hotmail.com>
2021-05-01 01:16:03 +0000

    backend

--- playground/release/server.js
+++ playground/release/server.js
@@ -1,25 +1,24 @@
 import express from 'express'
 import http from 'http'
 import createGame from './public/game.js'
-//import socketio from 'socket.io'
+import socketio from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
-//const sockets = socketio(server)
+const sockets = socketio(server)
 
 app.use(express.static('public'))
 
-const game = createGame()
-game.addPlayer({ playerId: 'player1', playerX: 1, playerY: 1})
-game.addFruit({fruitId: 'fruit1', fruitX: 4, fruitY: 3}) 
-game.movePlayer({playerId: 'player1', keyPressed: 'ArrowLeft'})          
+const game = createGame()       
 
 console.log(game.state)
 
-//sockets.on('connection', (socket) => {
-//    const playerId = socket.id
-//    console.log(`Player connect on Server with id ${playerId}`)
-//}) 
+sockets.on('connection', (socket) => {
+    const playerId = socket.id
+    console.log(`Player connect on Server with id ${playerId}`)
+
+    socket.emit('setup', game.state)
+}) 
 
 server.listen(3000, () => {
     console.log(`Server listening on port: 3000`)
