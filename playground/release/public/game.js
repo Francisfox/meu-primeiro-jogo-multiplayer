20bd0c833e23ea018213e74324024dc2a8659adc
Francisfox <francismarfox@hotmail.com>
2021-05-09 18:55:41 +0000

    testend

--- playground/release/public/game.js
+++ playground/release/public/game.js
@@ -67,6 +67,7 @@ export default function createGame(){
     }
 
     function movePlayer(command){
+        notifyAll(command)
          
         const acceptedMoves = {
             ArrowUp(player) {
